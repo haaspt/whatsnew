79152c0b4166e4e4b6cb9debfbe4222ce6ec04dd
haaspt <patrick.tyler.haas@gmail.com>
2015-09-03 11:55:15 -0700

    Initial commit

---
+++ filters.py
@@ -0,0 +1,17 @@
+nyt_filter = ['Business', 'Business Day', 'Front Page', 'International Home', 'N.Y. / Region', 'N.Y./Region',
+              'NYRegion', 'NYT Now', 'National', 'New York', 'New York and Region', 'Science', 'Technology',
+              "Today's Headlines", 'U.S.', 'Washington', 'World']
+
+"""List of possible values for section filter:
+Arts, Automobiles, Autos, Blogs, Books, Booming, Business, Business Day, 
+Corrections, Crosswords & Games, Crosswords/Games, Dining & Wine, Dining and Wine, 
+Editors' Notes, Education, Fashion & Style, Food, Front Page, Giving, Global Home, 
+Great Homes & Destinations, Great Homes and Destinations, Health, Home & Garden, 
+Home and Garden, International Home, Job Market, Learning, Magazine, Movies, Multimedia, 
+Multimedia/Photos, N.Y. / Region, N.Y./Region, NYRegion, NYT Now, National, New York, 
+New York and Region, Obituaries, Olympics, Open, Opinion, Paid Death Notices, 
+Public Editor, Real Estate, Science, Sports, Style, Sunday Magazine, Sunday Review, 
+T Magazine, T:Style, Technology, The Public Editor, The Upshot, Theater, Times Topics, 
+TimesMachine, Today's Headlines, Topics, Travel, U.S., Universal, UrbanEye, Washington, 
+Week in Review. World, Your Money
+"""
