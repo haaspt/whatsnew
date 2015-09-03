4762d95bd97e6d22782762beac5d5b776c800555
haaspt <patrick.tyler.haas@gmail.com>
2015-09-03 11:55:43 -0700

    Filters now imported from filters.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import click
+from filters import nyt_filter
 
 """
 For NYT API documentation see: http://developer.nytimes.com/io-docs and http://developer.nytimes.com/docs/times_newswire_api/
@@ -12,24 +13,6 @@ click.clear()
 raw_news = requests.get('http://api.nytimes.com/svc/news/v3/content/all/all/.json?limit=20&api-key=cb6b696a018707793e9436bda629bde3%3A4%3A66817738')
 
 news = raw_news.json()
-
-nyt_filter = ['Business', 'Business Day', 'Front Page', 'International Home', 'N.Y. / Region', 'N.Y./Region',
-              'NYRegion', 'NYT Now', 'National', 'New York', 'New York and Region', 'Science', 'Technology',
-              "Today's Headlines", 'U.S.', 'Washington', 'World']
-
-"""List of possible values for section filter:
-Arts, Automobiles, Autos, Blogs, Books, Booming, Business, Business Day, 
-Corrections, Crosswords & Games, Crosswords/Games, Dining & Wine, Dining and Wine, 
-Editors' Notes, Education, Fashion & Style, Food, Front Page, Giving, Global Home, 
-Great Homes & Destinations, Great Homes and Destinations, Health, Home & Garden, 
-Home and Garden, International Home, Job Market, Learning, Magazine, Movies, Multimedia, 
-Multimedia/Photos, N.Y. / Region, N.Y./Region, NYRegion, NYT Now, National, New York, 
-New York and Region, Obituaries, Olympics, Open, Opinion, Paid Death Notices, 
-Public Editor, Real Estate, Science, Sports, Style, Sunday Magazine, Sunday Review, 
-T Magazine, T:Style, Technology, The Public Editor, The Upshot, Theater, Times Topics, 
-TimesMachine, Today's Headlines, Topics, Travel, U.S., Universal, UrbanEye, Washington, 
-Week in Review. World, Your Money
-"""
 
 def default_display():
     index_num = 0
