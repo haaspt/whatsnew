1643c79f2d420f28d930844f750d5d3b4260b3f3
haaspt <patrick.tyler.haas@gmail.com>
2015-09-16 16:40:40 -0700

    Added setup.py

---
+++ build/scripts-2.7/main.py
@@ -0,0 +1,77 @@
+import click
+import newsfeeds
+import random
+from config import GlobalConfig
+
+
+click.echo("Loading the news...")
+
+option = GlobalConfig()
+
+story_list = newsfeeds.feeder()
+
+click.clear()
+
+def mixer(full_story_list, sample_number):
+    """Selects a random sample of stories from the full list to display to the user.
+    Number of stories is set in config.py
+    Todo: Add argument support for number of stories to display
+    """
+
+    mixed_story_list = random.sample(set(full_story_list), sample_number)
+    return mixed_story_list
+
+
+def default_display(list_of_stories):
+    """Displays a set of stories in the following format:
+    n - Story Title -- OutletName -- Section 
+    Story abstract: Lorem ipsum dolor sit amet
+    """
+
+    index_num = 0
+    for story in list_of_stories:
+        index_num += 1
+        click.secho('%r - ' % index_num, bold=True, nl=False)
+        click.secho('%s ' % story.title, fg=option.headline_color, bold=True, nl=False)
+        click.secho('-- %s -- ' % story.source, fg=option.source_color, bold=True, nl=False)
+        click.secho('%s' % story.section, fg=option.section_color)
+        click.secho('Story abstract: %s' % story.abstract, fg=option.abstract_color)
+        click.echo()
+    
+    if index_num > 0:
+        exit == False
+        while exit != True:
+            click.secho("Select an index number to go to story, or [Enter] to exit: ", fg=option.prompt_color, bold=True, nl=False)
+            raw_selection = raw_input()
+            if raw_selection.isdigit():
+                selection = int(raw_selection) - 1
+                if selection <= index_num - 1:
+                    story = mixed_story_list[selection]
+                    click.launch(story.url)
+                    if option.prompt_until_exit == True:
+                        pass
+                    else:
+                        return exit == True
+                else:
+                    click.secho("Invalid entry", fg='red')
+                    if option.prompt_until_exit == True:
+                        pass
+                    else:
+                        return exit == True
+
+            elif raw_selection == '':
+                return exit == True
+
+            else:
+                click.secho("Invalid entry", fg='red')
+                if option.prompt_until_exit == True:
+                    pass
+                else:
+                    return exit == True
+                
+        else:
+            click.secho("No recent headlines to display", fg=option.prompt_color, bold=True, nl=False)
+            click.echo()
+
+mixed_story_list = mixer(story_list, option.article_limit)
+default_display(mixed_story_list)
---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+readme = open('README.md').read()
+
+requirements = ['click', 'feedparser', 'BeautifulSoup']
+
+setup(
+    name = "whatsnew",
+    version = "0.12",
+    author = "Patrick Tyler Haas",
+    author_email = "patrick.tyler.haas@gmail.com",
+    description = ("A lightweight, convenient tool to get an overview of the day's headlines right from your command line."),
+    license = "MIT",
+    keywords = "",
+    url = "https://github.com/haaspt/whatsnew",
+    scripts=['main.py', 'newsfeeds.py', 'config.py'],
+    install_requires=requirements,
+    long_description=readme,
+    entry_points = {
+        'console_scripts': [
+            'whatsnew = main'
+            ],
+        },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        ],
+    )
