678a5e988c5b314e6db96a81f10162407eea6db8
haaspt <patrick.tyler.haas@gmail.com>
2015-11-14 12:18:54 -0800

    setup: added entry_points syntax (broken)

--- setup.py
+++ setup.py
@@ -17,6 +17,11 @@ setup(
     scripts=['main.py', 'newsfeeds.py', 'config.py'],
     install_requires=requirements,
     long_description=readme,
+    entry_points = {
+        'console_scripts': [
+            'whatsnew = main'
+            ],
+        },
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
